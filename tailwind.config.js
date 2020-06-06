47ee776d23839ba223c4d8c6024bcda7c75967aa
Vic Vijayakumar <needcaffeine@gmail.com>
2020-06-06 01:50:00 -0400

    new(post): new blog post on whiteboarding interviews

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-    purge: ['./components/**/*.js', './pages/**/*.js'],
+    purge: ['./components/**/*.js', './pages/**/*.js', './_content/**/*.md'],
     theme: {
         extend: {
             fontFamily: {
