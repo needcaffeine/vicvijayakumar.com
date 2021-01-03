695d8be1f4483628f489a4910289708bd07c85cd
Vic Vijayakumar <needcaffeine@gmail.com>
2021-01-03 00:44:56 -0500

    new: add more projects to project page

---
+++ components/Project.js
@@ -0,0 +1,64 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const ucfirst = (string) => string && string.charAt(0).toUpperCase() + string.slice(1)
+
+function Status({ status }) {
+    let statusClasses
+
+    switch (status) {
+        case 'inactive':
+            statusClasses = 'bg-red-100 text-red-800'
+            break
+
+        default:
+            statusClasses = 'bg-green-100 text-green-800'
+            break
+    }
+
+    return (
+        <span
+            className={`inline-flex items-center px-2 py-0.5 rounded text-xs font-medium ${statusClasses}`}
+        >
+            {ucfirst(status)}
+        </span>
+    )
+}
+
+Status.propTypes = {
+    status: PropTypes.string.isRequired,
+}
+
+function Project({ image, title, description, status, url }) {
+    return (
+        <div className="flex flex-col overflow-hidden rounded-lg shadow-lg">
+            <div className="flex-shrink-0">
+                <img className="object-cover w-full h-48" src={image} alt="" />
+            </div>
+            <div className="flex flex-col justify-between flex-1 p-6 dark:bg-gray-800">
+                <div className="flex-1">
+                    <a
+                        href={url}
+                        rel="nofollow noreferrer"
+                        className="text-xl font-semibold text-blue-500 hover:text-blue-400"
+                    >
+                        {title}
+                    </a>
+                    <p className="mt-3 text-base">{description}</p>
+
+                    <Status status={status} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+Project.propTypes = {
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+}
+
+export default Project
--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Project from 'components/Project'
 import DefaultLayout from 'components/Layout/DefaultLayout'
 
 export default function Projects() {
@@ -19,32 +20,32 @@ export default function Projects() {
                         time.
                     </p>
 
-                    <div className="grid max-w-lg gap-5 mt-6 lg:grid-cols-3 lg:max-w-none">
-                        <div className="flex flex-col overflow-hidden rounded-lg shadow-lg">
-                            <div className="flex-shrink-0">
-                                <img
-                                    className="object-cover w-full h-48"
-                                    src="https://images.unsplash.com/photo-1599986905849-ae1d81c10843?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80"
-                                    alt=""
-                                />
-                            </div>
-                            <div className="flex flex-col justify-between flex-1 p-6 dark:bg-gray-800">
-                                <div className="flex-1">
-                                    <a
-                                        href="https://www.everyoak.com"
-                                        rel="nofollow noreferrer"
-                                        className="text-xl font-semibold text-blue-500 hover:text-blue-400"
-                                    >
-                                        Everyoak
-                                    </a>
-                                    <p className="mt-3 text-base">
-                                        Everyoak is a preschool management system that helps schools
-                                        move all their paperwork online. Everyoak saves huge amounts
-                                        of time for both schools as well as caregivers.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
+                    <div className="grid max-w-lg gap-5 mt-6 md:grid-cols-3 md:max-w-none">
+                        <Project
+                            title="Everyoak"
+                            description="Everyoak is a preschool management system that helps schools move all
+                            their paperwork online. Everyoak saves huge amounts of time for both
+                            schools as well as caregivers."
+                            url="https://www.everyoak.com/"
+                            status="active"
+                            image="https://images.unsplash.com/photo-1599986905849-ae1d81c10843?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80"
+                        />
+
+                        <Project
+                            title="Dog Ears"
+                            description="Dog Ears helps Twitter users instantly search their likes and retweets."
+                            url="https://dogears.xyz/"
+                            status="inactive"
+                            image="https://images.unsplash.com/photo-1516726283839-a493d9f167aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+                        />
+
+                        <Project
+                            title="Heimdall"
+                            description="Heimdall is a program that allows you to programmatically connect to different AWS EC2 hosts via a Bastion/Jump host."
+                            url="https://github.com/needcaffeine/heimdall"
+                            status="active"
+                            image="https://images.unsplash.com/photo-1564159716326-6f5fcc32ba05?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80"
+                        />
                     </div>
                 </div>
             </div>
