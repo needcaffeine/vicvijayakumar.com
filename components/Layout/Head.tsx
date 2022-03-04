07392c3be862455201862a23f74f692eecdb2c8e
Vic Vijayakumar <needcaffeine@gmail.com>
2022-03-04 17:11:10 -0500

    chore: update outdated deps

--- components/Layout/Head.tsx
+++ components/Layout/Head.tsx
@@ -1,6 +1,7 @@
 /* eslint react/no-danger: 0 */
 import PropTypes from 'prop-types'
 import NextHead from 'next/head'
+import Script from 'next/script'
 import { DefaultSeo, NextSeo } from 'next-seo'
 
 import SEO from 'next-seo.config'
@@ -29,7 +30,6 @@ const Head = ({ description, title, url }) => {
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                 <link rel="icon" sizes="180x180" href="/apple-touch-icon.png" />
                 <link rel="manifest" href="/site.webmanifest" />
-                <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
                 <meta name="apple-mobile-web-app-title" content="Vic Vijayakumar" />
                 <meta name="application-name" content="Vic Vijayakumar" />
                 <link
@@ -49,11 +49,11 @@ const Head = ({ description, title, url }) => {
                     crossOrigin="anonymous"
                 />
 
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${tagManagerId}');`,
-                    }}
-                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${tagManagerId}');
+                    `}
+                </Script>
                 <noscript
                     dangerouslySetInnerHTML={{
                         __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${tagManagerId}" height="0" width="0" style="display:none;visibility:hidden" />`,
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,9 @@ export default class MyDocument extends Document {
     render() {
         return (
             <Html className="bg-white dark:bg-gray-900" lang="en">
-                <Head />
+                <Head>
+                    <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+                </Head>
 
                 <body className="font-sans antialiased leading-normal text-gray-900 dark:text-white">
                     <Main />
