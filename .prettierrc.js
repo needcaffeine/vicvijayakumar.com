9bae510739487e713adffdc57e1cb90f3c95359c
Vic Vijayakumar <needcaffeine@gmail.com>
2023-11-02 20:42:58 -0400

    restore GA and plausible

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,7 +3,7 @@ module.exports = {
   printWidth: 100,
   semi: true,
   singleQuote: false,
-  tabWidth: 2,
+  tabWidth: 4,
   useTabs: false,
   plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss" /* Must come last */],
   overrides: [
--- astro.config.ts
+++ astro.config.ts
@@ -11,43 +11,43 @@ import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://vicvijayakumar.com/",
-	markdown: {
-		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
-		remarkRehype: { footnoteLabelProperties: { className: [""] } },
-		shikiConfig: {
-			theme: "dracula",
-			wrap: true,
-		},
-	},
-	integrations: [
-		mdx({}),
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		sitemap(),
-		prefetch(),
-	],
-	vite: {
-		plugins: [rawFonts([".ttf"])],
-		optimizeDeps: {
-			exclude: ["@resvg/resvg-js"],
-		},
-	},
+    site: "https://vicvijayakumar.com/",
+    markdown: {
+        remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+        remarkRehype: { footnoteLabelProperties: { className: [""] } },
+        shikiConfig: {
+            theme: "dracula",
+            wrap: true,
+        },
+    },
+    integrations: [
+        mdx({}),
+        tailwind({
+            applyBaseStyles: false,
+        }),
+        sitemap(),
+        prefetch(),
+    ],
+    vite: {
+        plugins: [rawFonts([".ttf"])],
+        optimizeDeps: {
+            exclude: ["@resvg/resvg-js"],
+        },
+    },
 });
 
 function rawFonts(ext: Array<string>) {
-	return {
-		name: "vite-plugin-raw-fonts",
-		// @ts-ignore:next-line
-		transform(_, id) {
-			if (ext.some((e) => id.endsWith(e))) {
-				const buffer = fs.readFileSync(id);
-				return {
-					code: `export default ${JSON.stringify(buffer)}`,
-					map: null,
-				};
-			}
-		},
-	};
+    return {
+        name: "vite-plugin-raw-fonts",
+        // @ts-ignore:next-line
+        transform(_, id) {
+            if (ext.some((e) => id.endsWith(e))) {
+                const buffer = fs.readFileSync(id);
+                return {
+                    code: `export default ${JSON.stringify(buffer)}`,
+                    map: null,
+                };
+            }
+        },
+    };
 }
